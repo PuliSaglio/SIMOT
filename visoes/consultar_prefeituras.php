7f3513fa7dd8ba6a27bfe78b535316d36a57a9ce
Sara-niery <nierysantos@hotmail.com>
2023-01-18 00:28:23 -0300

    login

--- visoes/consultar_prefeituras.php
+++ visoes/consultar_prefeituras.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prefeituras cadastradas</title>
-
+    <link rel="stylesheet" href="../templates/estilo.css">
     <style type = "text/css">
         h2{
             text-align: center;
